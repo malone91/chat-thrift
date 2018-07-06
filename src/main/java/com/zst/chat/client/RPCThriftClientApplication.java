4fba27471ad535d8786362f42927811945bd3ec0
malong <760098065@qq.com>
2018-07-06 10:39:51 +0800

    add poll thread

--- src/main/java/com/zst/chat/client/RPCThriftClientApplication.java
+++ src/main/java/com/zst/chat/client/RPCThriftClientApplication.java
@@ -1,7 +1,13 @@
 package com.zst.chat.client;
 
+import com.zst.chat.listen.ClientThread;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Ablert
@@ -13,5 +19,9 @@ public class RPCThriftClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RPCThriftClientApplication.class, args);
+        //启动线程池
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 3, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        executor.execute(new ClientThread());
+        executor.shutdown();
     }
 }
--- src/main/java/com/zst/chat/listen/ClientThread.java
+++ src/main/java/com/zst/chat/listen/ClientThread.java
@@ -1,14 +1,9 @@
 package com.zst.chat.listen;
 
-import com.zst.chat.service.*;
-import org.apache.thrift.TException;
-import org.apache.thrift.TProcessor;
-import org.apache.thrift.protocol.TBinaryProtocol;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Ablert
@@ -17,25 +12,19 @@ import org.slf4j.LoggerFactory;
  */
 public class ClientThread implements Runnable {
 
+    private static volatile boolean runningFlag = true;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ClientThread.class);
 
     @Override
     public void run() {
-        String ip = "10.1.26.190";
-        int port = 9091;
-        int timeOut = 1000;
-        //创建transport
-        TTransport transport = new TSocket(ip, port, timeOut);
-        //创建TProtocol
-        TProtocol protocol = new TBinaryProtocol(transport);
-        //基于transport 和 protocol创建client
-        TProcessor processor = new CallbackService.Processor(new CallBackServiceImpl());
-        try {
-            while (true) {
-                processor.process(protocol, protocol);
+        while (runningFlag) {
+            System.out.println("正在读取数据");
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                LOGGER.error("ClientThread run error", e);
             }
-        } catch (TException e) {
-            e.printStackTrace();
         }
     }
 }
